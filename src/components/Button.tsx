d1c4bc2978574fa224a730ae963afd1d7c5382d6
Rolfzy <89122134+rolfzy@users.noreply.github.com>
2025-03-15 00:59:32 +0700

    Update Responsive & Link Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,10 @@
 export default function Button({ text }: { text: string }) {
+  const handleClick = () => {
+    window.open('https://chromewebstore.google.com/detail/remindeen/hoaielmggcbaiaomgmieobdloimgphio?utm_source=item-share-cb')
+  };
   return (
     <div className='bg-white text-slate-900 rounded-full px-4 py-1 text-center'>
-      <button className='text-lg cursor-pointer sm:text-lg'>{text}</button>
+      <button className='text-lg cursor-pointer sm:text-lg' onClick={handleClick}>{text}</button>
     </div>
   );
 }
