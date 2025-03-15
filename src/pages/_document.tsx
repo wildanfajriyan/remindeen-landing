4f0c320e889c9b97541422bdbe242dcea50793f5
wildanuwil <wildanfajriyann@gmail.com>
2025-03-15 13:40:05 +0700

    adjust title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+      <link rel="icon" href="/remindeen-logo.png" sizes="any" />
+        <title>Remindeen</title>
+      </Head>
       <body className='antialiased'>
         <Main />
         <NextScript />
